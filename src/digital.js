680a9e5c764908d91d1cf6455409e568b4873cda
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2021-10-22 16:24:26 +0100

    style changes digital

--- src/digital.js
+++ src/digital.js
@@ -5,16 +5,27 @@ import './digital.css'
 function DigitalBase() {
 
     const time = useContext(TimeContext)
+    const [style,setstyle] = useState({})
 
     const format = (n) => {
         return n < 10 ? "0"+n : n
     }
 
-    
+    const span = <span style={style}>:</span>
+
+    useEffect(()=> {
+        if (time.seconds % 2 === 0 ) {
+            setstyle({color:'rgba(255,255,255,0.5)'})
+        } else {
+            setstyle({})
+        }
+    },[time])
+
     return (
        <div className="digital-base">
            <div className="digital-time">
-                <h1>{`${format(time.hours)} : ${format(time.minutes)} : ${format(time.seconds)}`}</h1>
+               <h1> {format(time.hours)} {span} {format(time.minutes)} {span} {format(time.seconds)}</h1>
+                {/* <h1>{`${format(time.hours)} ${span} ${format(time.minutes)} : ${format(time.seconds)}`}</h1> */}
             </div>
        </div>
     )

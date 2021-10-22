6e47ce7a43e8bbd3c386cffde339cf5e5f277fd0
rafisfast <tah18245393@students.wolvcoll.ac.uk>
2021-10-22 15:23:23 +0100

    analog style changes

--- src/analog.js
+++ src/analog.js
@@ -11,13 +11,13 @@ function Needle() {
 
     return (
         <div className="bucket">
-            <div className="second-hand" style={{transform: 'rotate(' + seconds/60 * 360 + 'deg)'}}>
+            <div className="second hand" style={{transform: 'rotate(' + seconds/60 * 360 + 'deg)'}}>
                 {/* {seconds} */}
             </div>
-            <div className="hour-hand" style={{transform: 'rotate(' + hours/12 * 360 + 'deg)'}}>
+            <div className="hour hand" style={{transform: 'rotate(' + hours/12 * 360 + 'deg)'}}>
                 {/* {hours - 12} */}
             </div>
-            <div className="minute-hand" style={{transform: 'rotate(' + minutes/60 * 360 + 'deg)'}}>
+            <div className="minute hand" style={{transform: 'rotate(' + minutes/60 * 360 + 'deg)'}}>
                 {/* {minutes} */}
             </div>
         </div>
